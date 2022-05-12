01589f724bc52de6e5356aad47e7c5069a62e30d
Rafael <rafinhabzf@gmail.com>
2022-05-12 19:16:35 -0300

    Resolvido: Vamos Praticar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/variaveis/scripts.js
@@ -0,0 +1,10 @@
+const myName = "Rafael";
+const birthCity = "São Joaquim";
+let birthYear = 2003;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);

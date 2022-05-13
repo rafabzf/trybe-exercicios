99dede602ab4c249e94926e9df9b8da2fc9654da
Rafael <rafinhabzf@gmail.com>
2022-05-13 19:48:51 -0300

    Feito exercicio vamos vamos praticar

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/switch-e-case/scripts.js
@@ -0,0 +1,18 @@
+let estadoCandidata = "aprovada";
+
+switch (estadoCandidata) {
+    case "aprovada":
+        console.log("aprovada");
+        break;
+    
+    case "lista":
+        console.log("lista");
+        break;
+
+    case "reprovada":
+        console.log("reprovada");
+        break;
+
+    default:
+        console.log("não se aplica");
+}

063e1edc2dbb927333737b4c6143cf9db99b728e
Rafael <rafinhabzf@gmail.com>
2022-05-17 15:14:51 -0300

    Feito exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,6 @@
+let fatorialDez = 1;
+
+for (let index = 10; index > 0; index -= 1) {
+    fatorialDez *= index;
+}
+console.log(fatorialDez);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,7 @@
+let word = 'rafael';
+let palavraInvertida = '';
+
+for (let index = 0; index < word.length; index += 1) {
+    palavraInvertida += word[word.length - 1 - index];
+}
+console.log(palavraInvertida);

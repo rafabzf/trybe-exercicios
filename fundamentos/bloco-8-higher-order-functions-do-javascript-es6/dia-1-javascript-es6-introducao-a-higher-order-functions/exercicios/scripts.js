b5f003e2f8c2027446feb84c22c7dad8f12b0758
Rafael <rafinhabzf@gmail.com>
2022-06-22 19:22:49 -0300

    Feito exercicio 2

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/scripts.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicios/scripts.js
@@ -1,15 +1,8 @@
-const newEmployees = (employeeFunction) => {
-    const employees = {
-      id1: employeeFunction('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: employeeFunction('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: employeeFunction('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
-    }
-    return employees;
-  };
+const checarResultado = (meuNumero, numero) => meuNumero === numero;
 
-const employeeFunction = (name) => {
-    const email = name.toLowerCase().split(' ').join('_');
-    return {name, email: `${email}@betrybe.com`};
+const resultado = (meuNumero, callback) => {
+  const numero = Math.floor((Math.random() * 5) + 1);
+
+  return callback(meuNumero, numero) ? 'Parabéns você ganhou' : 'Tente novamente';
 }
-
-console.log(newEmployees(employeeFunction));
+console.log(resultado(4, checarResultado));

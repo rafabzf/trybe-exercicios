8c255f9e0f7a77b3ebcb542c3c81bf44f85bc006
Rafael <rafinhabzf@gmail.com>
2022-06-24 10:48:25 -0300

    Feito exercicios

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise3.js
@@ -0,0 +1,15 @@
+const expectedResult = {
+    author: {
+      birthYear: 1948,
+      name: 'George R. R. Martin',
+    },
+    genre: 'Fantasia',
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    releaseYear: 1991,
+  };
+  
+  function getNamedBook() {
+    // escreva seu código aqui
+    return books.find((book) => book.name.length === 26);
+  }

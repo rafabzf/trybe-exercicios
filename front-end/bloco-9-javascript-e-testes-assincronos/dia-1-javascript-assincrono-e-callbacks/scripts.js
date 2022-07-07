7d79c2ccd018794794ff27594a31bf8b35c2301a
Rafael <rafinhabzf@gmail.com>
2022-07-07 10:52:38 -0300

    Exercicio 4

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/scripts.js
@@ -1,11 +1,13 @@
-const getPlanet = () => {
-    const mars = {
-      name: "Mars",
-      distanceFromSun: {
-        value: 227900000,
-        measurementUnit: "kilometers",
-      },
-    };
-    setTimeout(() => console.log("Returned planet: ", mars), 4000);
-  };
- getPlanet(); // imprime Marte depois de 4 segundos
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+    setTimeout(() => console.log(`Mars temperature is: ${getMarsTemperature()} degree Celsius`), messageDelay());
+}
+
+sendMarsTemperature();
